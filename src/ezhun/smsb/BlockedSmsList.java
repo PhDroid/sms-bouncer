2ef8fac0953bb66aa6c19bd6b395d979a1da925e
Max Korenkov <mkorenkov@gmail.com>
2011-03-03 01:09:07 +0200

    idea project created

---
+++ src/ezhun/smsb/BlockedSmsList.java
@@ -0,0 +1,15 @@
+package ezhun.smsb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlockedSmsList extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
