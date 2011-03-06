c2af2cab07bd8403ff98b86c7bc20861e8c48538
Max Korenkov <mkorenkov@gmail.com>
2011-03-06 23:00:05 +0200

    Activities stubs introduced

---
+++ src/ezhun/smsb/activity/BlockedSendersList.java
@@ -0,0 +1,13 @@
+package ezhun.smsb.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Shows black list of senders
+ */
+public class BlockedSendersList extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ src/ezhun/smsb/activity/BlockedSmsDetails.java
@@ -0,0 +1,13 @@
+package ezhun.smsb.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Shows Details information for the Blocked Sms.
+ */
+public class BlockedSmsDetails extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
