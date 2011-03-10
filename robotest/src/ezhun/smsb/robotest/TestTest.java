600bd2d3cefceabbd539d98ad1c48c634630cc44
Max Korenkov <mkorenkov@gmail.com>
2011-03-10 19:08:25 +0200

    added test project from Eclipse, which works

---
+++ robotest/src/ezhun/smsb/robotest/TestTest.java
@@ -0,0 +1,16 @@
+package ezhun.smsb.robotest;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+
+public class TestTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+       Assert.assertTrue(1 + 1 == 2);
+    }
+
+    public void testSomethingElse() throws Throwable {
+       Assert.assertTrue(1 + 1 == 3);
+    }
+}
