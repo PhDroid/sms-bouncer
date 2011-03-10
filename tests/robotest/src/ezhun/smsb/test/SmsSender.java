33d322aa109246ad58cb59477b1ea9fd6de6769b
Max Korenkov <mkorenkov@gmail.com>
2011-03-11 00:01:45 +0200

    Implemented Telnet client and SmsSending mechanism for test

---
+++ tests/robotest/src/ezhun/smsb/test/SmsSender.java
@@ -0,0 +1,38 @@
+package ezhun.smsb.test;
+
+public class SmsSender {
+	private String server = "127.0.0.1";
+	private int port = 5554;
+	
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void setServer(String server) {
+		this.server = server;
+	}
+	
+	public String getServer() {
+		return server;
+	}
+
+	public void sendSms(String from, String message) {
+		Telnet t = new Telnet(this.server, this.port);
+		t.sendCommand("sms send " + from + " " + message);
+		t.disconnect();
+	}
+
+	public static void main(String[] args) {
+		try {
+			SmsSender s = new SmsSender();
+			s.sendSms("380971123326", "test message from SmsSender");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
