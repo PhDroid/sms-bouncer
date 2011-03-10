bf6241adfea7797d2c34c341795780756f71bc19
Max Korenkov <mkorenkov@gmail.com>
2011-03-10 02:24:57 +0200

    test project setup

---
+++ robotest/src/ezhun/smsb/robotest/activity/BlockedSmsListTest.java
@@ -0,0 +1,22 @@
+package ezhun.smsb.robotest.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ezhun.smsb.activity.BlockedSmsList;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class ezhun.smsb.activity.BlockedSmsListTest \
+ * ezhun.smsb.tests/android.test.InstrumentationTestRunner
+ */
+public class BlockedSmsListTest extends ActivityInstrumentationTestCase2<BlockedSmsList> {
+
+    public BlockedSmsListTest() {
+        super("ezhun.smsb", BlockedSmsList.class);
+    }
+
+}
