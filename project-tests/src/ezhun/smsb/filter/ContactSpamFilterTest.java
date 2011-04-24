740e1f8f67e9d534dcce74011389de31d9425340
Max Korenkov <mkorenkov@gmail.com>
2011-04-25 01:49:28 +0300

    creating sample contacts

--- project-tests/src/ezhun/smsb/filter/ContactSpamFilterTest.java
+++ project-tests/src/ezhun/smsb/filter/ContactSpamFilterTest.java
@@ -1,18 +1,44 @@
 package ezhun.smsb.filter;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.ContentResolver;
 import android.provider.ContactsContract;
+import android.test.AndroidTestCase;
 import android.test.ProviderTestCase2;
+
+import java.util.ArrayList;
 
 /**
  * Test class for ContactSpamFilter.
  */
-public class ContactSpamFilterTest extends ProviderTestCase2 {
-	public ContactSpamFilterTest() {
-		super(ContactsContract.Contacts.class, ContactsContract.Contacts.CONTENT_URI.getAuthority());
+public class ContactSpamFilterTest extends AndroidTestCase {
+	private ContentResolver getContentResolver() {
+		return this.getContext().getContentResolver();
 	}
 
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
+
+		ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+		int rawContactInsertIndex = ops.size();
+
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+				.withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+				.withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+				.build());
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+				.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+				.withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, "9X-XXXXXXXXX")
+				.build());
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+				.withValue(ContactsContract.Data.MIMETYPE,
+						ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+				.withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "Mike Sullivan")
+				.build());
+		ContentProviderResult[] res = getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
 	}
 }
