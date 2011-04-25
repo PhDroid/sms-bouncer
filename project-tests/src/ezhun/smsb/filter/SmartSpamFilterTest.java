c4879f08bdfbc38d2b868e599dfbb4a7d164f3ce
Max Korenkov <mkorenkov@gmail.com>
2011-04-26 02:11:26 +0300

    WhiteListSpamFilter tests implemented. Not passed yet.

--- project-tests/src/ezhun/smsb/filter/SmartSpamFilterTest.java
+++ project-tests/src/ezhun/smsb/filter/SmartSpamFilterTest.java
@@ -1,7 +1,54 @@
 package ezhun.smsb.filter;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.provider.ContactsContract;
+import android.test.AndroidTestCase;
+
+import java.util.ArrayList;
+
 /**
  * Test class for SmartSpamFilter.
  */
-public class SmartSpamFilterTest {
+public class SmartSpamFilterTest extends AndroidTestCase {
+	private static String SENDER = "(067) 129-09-45";
+
+	private ContentResolver getContentResolver() {
+		return this.getContext().getContentResolver();
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		ArrayList<ContentProviderOperation> ops =
+				new ArrayList<ContentProviderOperation>();
+
+		int rawContactInsertIndex = ops.size();
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+				.withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, "com.ezhun.smsb")
+				.withValue(ContactsContract.RawContacts.ACCOUNT_NAME, "mike_sullivan")
+				.build());
+
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+				.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+				.withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, SENDER)
+				.build());
+		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+				.withValue(ContactsContract.Data.MIMETYPE,
+						ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+				.withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "Mike Sullivan")
+				.build());
+
+		getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		getContentResolver().delete(ContactsContract.RawContacts.CONTENT_URI, null, null);
+	}
 }
--- project-tests/src/ezhun/smsb/filter/WhiteListSpamFilterTest.java
+++ project-tests/src/ezhun/smsb/filter/WhiteListSpamFilterTest.java
@@ -1,7 +1,67 @@
 package ezhun.smsb.filter;
 
+import android.content.ContentResolver;
+import android.telephony.SmsManager;
+import android.test.AndroidTestCase;
+import ezhun.smsb.SmsPojo;
+import ezhun.smsb.exceptions.ApplicationException;
+import ezhun.smsb.storage.SmsContentProvider;
+import junit.framework.Assert;
+
 /**
  * Test class for WhiteListSpamFilter.
  */
-public class WhiteListSpamFilterTest {
+public class WhiteListSpamFilterTest extends AndroidTestCase {
+	private static String SENDER = "(097) 112 33 26";
+
+	private ContentResolver getContentResolver() {
+		return this.getContext().getContentResolver();
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		SmsPojo spam = new SmsPojo();
+		spam.setSender("1346");
+		spam.setMessage("Novaja aktsia ot magazinov Kharkova");
+		spam.setReceived((int) (System.currentTimeMillis() / 1000L));
+		getContentResolver().insert(SmsContentProvider.CONTENT_URI, spam.toContentValues());
+
+		SmsPojo notSpam = new SmsPojo();
+		notSpam.setSender(SENDER);
+		notSpam.setMessage("Let's grab some whisky");
+		notSpam.setReceived((int) (System.currentTimeMillis() / 1000L));
+		notSpam.setMarkedNotSpamByUser(true);
+		getContentResolver().insert(SmsContentProvider.CONTENT_URI, notSpam.toContentValues());
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		getContentResolver().delete(SmsContentProvider.CONTENT_URI, null, null);
+	}
+
+	public void testWhiteList() throws ApplicationException {
+		SmsPojo spam1 = new SmsPojo();
+		spam1.setSender("1346");
+		spam1.setMessage("Novaja aktsia ot magazinov Kharkova");
+		spam1.setReceived((int) (System.currentTimeMillis() / 1000L));
+
+		SmsPojo spam2 = new SmsPojo();
+		spam2.setSender("1343");
+		spam2.setMessage("Staraja aktsia ot magazinov Kharkova");
+		spam2.setReceived((int) (System.currentTimeMillis() / 1000L));
+
+		SmsPojo notSpam = new SmsPojo();
+		notSpam.setSender(SENDER);
+		notSpam.setMessage("Let's grab some whisky again");
+		notSpam.setReceived((int) (System.currentTimeMillis() / 1000L));
+
+		ISpamFilter filter = new WhiteListSpamFilter(getContentResolver());
+		Assert.assertEquals(true, filter.isSpam(spam1));
+		Assert.assertEquals(true, filter.isSpam(spam2));
+		Assert.assertEquals(false, filter.isSpam(notSpam));
+	}
 }
