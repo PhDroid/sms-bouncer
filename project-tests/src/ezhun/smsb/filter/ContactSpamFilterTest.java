bc4751f75de42092ce3e7d89a5235e6856ff3ebc
Max Korenkov <mkorenkov@gmail.com>
2011-04-25 11:52:40 +0300

    Implemented tests for ContactSpamFilter

--- project-tests/src/ezhun/smsb/filter/ContactSpamFilterTest.java
+++ project-tests/src/ezhun/smsb/filter/ContactSpamFilterTest.java
@@ -6,6 +6,8 @@ import android.content.ContentResolver;
 import android.provider.ContactsContract;
 import android.test.AndroidTestCase;
 import android.test.ProviderTestCase2;
+import ezhun.smsb.SmsPojo;
+import junit.framework.Assert;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,8 @@ import java.util.ArrayList;
  * Test class for ContactSpamFilter.
  */
 public class ContactSpamFilterTest extends AndroidTestCase {
+	private static String SENDER = "(067) 129-09-45";
+
 	private ContentResolver getContentResolver() {
 		return this.getContext().getContentResolver();
 	}
@@ -31,7 +35,7 @@ public class ContactSpamFilterTest extends AndroidTestCase {
 		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
 				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
 				.withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-				.withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, "9X-XXXXXXXXX")
+				.withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, SENDER)
 				.build());
 		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
 				.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
@@ -40,5 +44,34 @@ public class ContactSpamFilterTest extends AndroidTestCase {
 				.withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "Mike Sullivan")
 				.build());
 		ContentProviderResult[] res = getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		getContentResolver().delete(ContactsContract.RawContacts.CONTENT_URI, null, null);
+	}
+
+	public void testKnownNumber() throws Exception {
+		SmsPojo message = new SmsPojo();
+		message.setSender(SENDER);
+		message.setReceived((int) (System.currentTimeMillis() / 1000L));
+		message.setMessage("I am not a SPAM message");
+
+		ISpamFilter filter = new ContactSpamFilter(getContentResolver());
+		boolean isSpam = filter.isSpam(message);
+		Assert.assertEquals(false, isSpam);
+	}
+
+	public void testUnknownNumber() throws Exception {
+		SmsPojo message = new SmsPojo();
+		message.setSender("(097) 112-33-26");
+		message.setReceived((int) (System.currentTimeMillis() / 1000L));
+		message.setMessage("I am SPAM message");
+
+		ISpamFilter filter = new ContactSpamFilter(getContentResolver());
+		boolean isSpam = filter.isSpam(message);
+		Assert.assertEquals(true, isSpam);
 	}
 }
