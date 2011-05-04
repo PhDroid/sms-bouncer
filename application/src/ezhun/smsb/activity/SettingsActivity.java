5adc41929bab1c9195df35863a286ddd0aeb7c09
Max Korenkov <mkorenkov@gmail.com>
2011-05-04 21:59:10 +0300

    empty activities added

---
+++ application/src/ezhun/smsb/activity/SettingsActivity.java
@@ -0,0 +1,13 @@
+package ezhun.smsb.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Settings of the SMS bouncer application.
+ */
+public class SettingsActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
