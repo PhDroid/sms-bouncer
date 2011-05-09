49c5c899917e7a03fe1a607749458643e0951573
unkot <anatoly.kotlyar@gmail.com>
2011-05-10 01:22:33 +0300

    Merge branch 'master' of github.com:mkorenkov/sms-bouncer
    Conflicts:
    	application/gen/ezhun/smsb/R.java

--- application/gen/ezhun/smsb/R.java
+++ application/gen/ezhun/smsb/R.java
@@ -17,28 +17,25 @@ public final class R {
         public static final int icon=0x7f020003;
     }
     public static final class id {
+        public static final int btnEditWhitelist=0x7f06000e;
+        public static final int cbNotification=0x7f060008;
+        public static final int ddlClear=0x7f060004;
+        public static final int ddlDisplayNotification=0x7f06000c;
+        public static final int delete_all_item=0x7f060010;
+        public static final int linearLayout1=0x7f06000a;
+        public static final int list_menu_group=0x7f06000f;
         public static final int messageTextView=0x7f060003;
         public static final int messagesListView=0x7f060000;
         public static final int receivedTimeTextView=0x7f060002;
+        public static final int select_many_item=0x7f060011;
         public static final int senderTextView=0x7f060001;
-        public static final int widget31=0x7f060007;
-        public static final int widget32=0x7f060004;
-        public static final int btnEditWhitelist=0x7f06000b;
-        public static final int cbNotification=0x7f060005;
-        public static final int ddlClear=0x7f060001;
-        public static final int ddlDisplayNotification=0x7f060009;
-        public static final int delete_all_item=0x7f06000d;
-        public static final int linearLayout1=0x7f060007;
-        public static final int listViewSms=0x7f060000;
-        public static final int list_menu_group=0x7f06000c;
-        public static final int select_many_item=0x7f06000e;
-        public static final int settings_item=0x7f06000f;
-        public static final int tableRow1=0x7f060002;
-        public static final int tableRow2=0x7f060004;
-        public static final int tableRow3=0x7f060006;
-        public static final int tableRow4=0x7f06000a;
-        public static final int tbClear=0x7f060008;
-        public static final int tb_Heading=0x7f060003;
+        public static final int settings_item=0x7f060012;
+        public static final int tableRow1=0x7f060005;
+        public static final int tableRow2=0x7f060007;
+        public static final int tableRow3=0x7f060009;
+        public static final int tableRow4=0x7f06000d;
+        public static final int tbClear=0x7f06000b;
+        public static final int tb_Heading=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
