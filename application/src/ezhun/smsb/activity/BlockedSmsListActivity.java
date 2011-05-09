c65cfe49c4e83b21b574e9d141a92226bb3b0984
Max Korenkov <mkorenkov@gmail.com>
2011-05-09 20:38:52 +0300

    Settings menu action items

--- application/src/ezhun/smsb/activity/BlockedSmsListActivity.java
+++ application/src/ezhun/smsb/activity/BlockedSmsListActivity.java
@@ -1,15 +1,11 @@
 package ezhun.smsb.activity;
 
 import android.app.Activity;
-import android.database.DataSetObserver;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.MenuItem;
 import ezhun.smsb.R;
 
 public class BlockedSmsListActivity extends Activity {
@@ -27,10 +23,23 @@ public class BlockedSmsListActivity extends Activity {
 		super.onStart();
 	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.list_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.list_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.settings_item:
+				Intent intent = new Intent(BlockedSmsListActivity.this, SettingsActivity.class);
+				startActivity(intent);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
