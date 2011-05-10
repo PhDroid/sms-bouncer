00dc6ebe1144bd98ffbc270ff92a7336efd3cff3
Max Korenkov <mkorenkov@gmail.com>
2011-05-10 23:24:24 +0300

    edit whitelist activity

---
+++ application/src/ezhun/smsb/activity/EditWhitelistActivity.java
@@ -0,0 +1,15 @@
+package ezhun.smsb.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ezhun.smsb.R;
+
+/**
+ * Please, write short description of what this file is for.
+ */
+public class EditWhitelistActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editwhitelist);
+	}
+}
