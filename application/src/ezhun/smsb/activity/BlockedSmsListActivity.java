6a6953fad2de24adb046ebd996fa6ffb95df5256
unkot <anatoly.kotlyar@gmail.com>
2011-05-11 00:37:16 +0300

    Main list ui change

--- application/src/ezhun/smsb/activity/BlockedSmsListActivity.java
+++ application/src/ezhun/smsb/activity/BlockedSmsListActivity.java
@@ -42,43 +42,51 @@ public class BlockedSmsListActivity extends ListActivity {
         SmsPojo sms = new SmsPojo();
         sms.setMessage("Hey you there! How you doin'?");
         sms.setSender("+380971122333");
+        sms.setReceived(2000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("Nova aktsia vid magazinu Target! Kupuy 2 kartopli ta otrymay 1 v podarunok!");
         sms.setSender("TARGET");
+        sms.setReceived(10000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("This is my new number. B. Obama.");
         sms.setSender("+1570333444555");
+        sms.setReceived(15000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("How about having some beer tonight? Stranger.");
         sms.setSender("+380509998887");
         sms.setRead(true);
+        sms.setReceived(20000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("Vash rakhunok na 9.05.2011 stanovyt 27,35 uah.");
         sms.setSender("KYIVSTAR");
+        sms.setReceived(100000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("Skydky v ALDO. Kupuy 1 ked ta otrymuy dryguy v podarunok!");
         sms.setSender("ALDO");
+        sms.setReceived(2000000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("Big football tonight v taverne chili!");
         sms.setSender("+380991001010");
         sms.setRead(true);
+        sms.setReceived(2100000);
         messages.add(sms);
 
         sms = new SmsPojo();
         sms.setMessage("15:43 ZABLOKOVANO 17,99 UAH, SUPERMARKET PORTAL");
         sms.setSender("PUMB");
+        sms.setReceived(3000000);
         messages.add(sms);
 
         return messages;
