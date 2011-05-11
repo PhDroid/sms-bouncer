45483c5d6b0b34e8a542b50fa3c1361604609758
unkot <anatoly.kotlyar@gmail.com>
2011-05-11 19:44:36 +0300

    Main list design changes.

--- application/gen/ezhun/smsb/R.java
+++ application/gen/ezhun/smsb/R.java
@@ -14,42 +14,45 @@ public final class R {
         public static final int main_list_message_background=0x7f040000;
         public static final int main_list_message_text=0x7f040001;
         public static final int main_list_sender_text=0x7f040002;
-        public static final int message_background=0x7f040003;
-        public static final int message_text=0x7f040004;
-        public static final int received_text=0x7f040005;
-        public static final int sender_text=0x7f040006;
+        public static final int message_text=0x7f040003;
+        public static final int received_text=0x7f040004;
+        public static final int sender_text=0x7f040005;
     }
     public static final class drawable {
         public static final int ic_menu_block=0x7f020000;
         public static final int ic_menu_mark=0x7f020001;
         public static final int ic_menu_preferences=0x7f020002;
         public static final int icon=0x7f020003;
+        public static final int message_background=0x7f020004;
     }
     public static final class id {
-        public static final int btnEditWhitelist=0x7f07000e;
-        public static final int cbNotification=0x7f070008;
-        public static final int ddlClear=0x7f070004;
-        public static final int ddlDisplayNotification=0x7f07000c;
-        public static final int delete_all_item=0x7f070010;
-        public static final int linearLayout1=0x7f07000a;
-        public static final int list_menu_group=0x7f07000f;
-        public static final int messageTextView=0x7f070003;
-        public static final int messagesListView=0x7f070000;
-        public static final int receivedTimeTextView=0x7f070002;
-        public static final int select_many_item=0x7f070011;
-        public static final int senderTextView=0x7f070001;
-        public static final int settings_item=0x7f070012;
-        public static final int tableRow1=0x7f070005;
-        public static final int tableRow2=0x7f070007;
-        public static final int tableRow3=0x7f070009;
-        public static final int tableRow4=0x7f07000d;
-        public static final int tbClear=0x7f07000b;
-        public static final int tb_Heading=0x7f070006;
+        public static final int btnEditWhitelist=0x7f070010;
+        public static final int cbNotification=0x7f07000a;
+        public static final int ddlClear=0x7f070006;
+        public static final int ddlDisplayNotification=0x7f07000e;
+        public static final int delete_all_item=0x7f070012;
+        public static final int layout_editWhiteList=0x7f070000;
+        public static final int linearLayout1=0x7f07000c;
+        public static final int list_menu_group=0x7f070011;
+        public static final int list_whitelist=0x7f070001;
+        public static final int messageTextView=0x7f070005;
+        public static final int messagesListView=0x7f070002;
+        public static final int receivedTimeTextView=0x7f070004;
+        public static final int select_many_item=0x7f070013;
+        public static final int senderTextView=0x7f070003;
+        public static final int settings_item=0x7f070014;
+        public static final int tableRow1=0x7f070007;
+        public static final int tableRow2=0x7f070009;
+        public static final int tableRow3=0x7f07000b;
+        public static final int tableRow4=0x7f07000f;
+        public static final int tbClear=0x7f07000d;
+        public static final int tb_Heading=0x7f070008;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int main_list_item=0x7f030001;
-        public static final int settings=0x7f030002;
+        public static final int editwhitelist=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int main_list_item=0x7f030002;
+        public static final int settings=0x7f030003;
     }
     public static final class menu {
         public static final int list_menu=0x7f060000;
