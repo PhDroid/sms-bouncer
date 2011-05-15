9faf3ea0f38ccdbabc81c1ca003f8b53134bb5a6
Max Korenkov <mkorenkov@gmail.com>
2011-05-15 11:04:25 +0300

    Implemented tests for Application Settings persistence. Closes #17.

--- project-tests/src/ezhun/smsb/content/ApplicationSettingsTest.java
+++ project-tests/src/ezhun/smsb/content/ApplicationSettingsTest.java
@@ -3,15 +3,34 @@ package ezhun.smsb.content;
 import ezhun.smsb.base.MockedContextTestBase;
 import ezhun.smsb.storage.ApplicationSettings;
 import ezhun.smsb.storage.DefaultApplicationSettings;
+import ezhun.smsb.storage.DeleteAfter;
 import junit.framework.Assert;
 
 /**
  * Tests for ApplicationSettings class.
  */
 public class ApplicationSettingsTest extends MockedContextTestBase {
+	ApplicationSettings settings;
+
+	private ApplicationSettings getSettings() {
+		return settings;
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		settings = new ApplicationSettings(getMockContext());
+	}
+
 	public void test_ApplicationSettings_create_settings_file_with_default_values() {
-		ApplicationSettings s = new ApplicationSettings(getMockContext());
-		Assert.assertEquals(DefaultApplicationSettings.DELETE_MESSAGES_AFTER_VALUE, s.getDeleteAfter());
-		Assert.assertEquals(DefaultApplicationSettings.DISPLAY_NOTIFICATION_VALUE, s.getDisplayNotification());
+		Assert.assertEquals(DefaultApplicationSettings.DELETE_MESSAGES_AFTER_VALUE, getSettings().getDeleteAfter());
+		Assert.assertEquals(DefaultApplicationSettings.DISPLAY_NOTIFICATION_VALUE, getSettings().getDisplayNotification());
+	}
+
+	public void test_ApplicationSettings_update_settings_file() {
+		getSettings().setDeleteAfter(DeleteAfter.ThirtyDays);
+		getSettings().setDisplayNotification(false);
+		Assert.assertEquals(DeleteAfter.ThirtyDays, getSettings().getDeleteAfter());
+		Assert.assertEquals(false, getSettings().getDisplayNotification());
 	}
 }
