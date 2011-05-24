e13e9c3bb2124bc7704443cf53fdf94f94690258
Max Korenkov <mkorenkov@gmail.com>
2011-05-24 16:57:57 +0300

    Robotium added to project references

---
+++ project-tests/src/ezhun/smsb/activity/SettingsActivityTest.java
@@ -0,0 +1,15 @@
+package ezhun.smsb.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.jayway.android.robotium.solo.Solo;
+
+/**
+ * A test for settings activity..
+ */
+public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
+	private Solo solo;
+
+	public SettingsActivityTest() {
+		super(SettingsActivity.class);
+	}
+}
