aa1f23dcca2549593fc5ee5d0b25813e8c91823d
Max Korenkov <mkorenkov@gmail.com>
2011-05-24 19:22:38 +0300

    refactoring: IsolatedBroadcastContext and ResourcefulMockContext extracted from MockedContextTestBase

---
+++ project-tests/src/ezhun/smsb/base/util/IsolatedBroadcastContext.java
@@ -0,0 +1,48 @@
+package ezhun.smsb.base.util;
+
+import android.content.*;
+import android.content.pm.PackageManager;
+import android.test.IsolatedContext;
+import junit.framework.Assert;
+
+/**
+ * Broadcast Context isolated from the rest of the system.
+ */
+public class IsolatedBroadcastContext extends IsolatedContext {
+	Context broadcastContext;
+
+	public IsolatedBroadcastContext(ContentResolver resolver, Context targetContext, Context broadcastContext) {
+		super(resolver, targetContext);
+		this.broadcastContext = broadcastContext;
+	}
+
+	public Context getBroadcastContext() {
+		Assert.assertNotNull(broadcastContext);
+		return broadcastContext;
+	}
+
+	@Override
+	public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter) {
+		return getBroadcastContext().registerReceiver(receiver, filter);
+	}
+
+	@Override
+	public PackageManager getPackageManager() {
+		return getBroadcastContext().getPackageManager();
+	}
+
+	@Override
+	public void unregisterReceiver(BroadcastReceiver receiver) {
+		getBroadcastContext().unregisterReceiver(receiver);
+	}
+
+	@Override
+	public void sendOrderedBroadcast(Intent intent, String receiverPermission) {
+		getBroadcastContext().sendOrderedBroadcast(intent, receiverPermission);
+	}
+
+	@Override
+	public void sendBroadcast(Intent intent, String receiverPermission) {
+		getBroadcastContext().sendBroadcast(intent, receiverPermission);
+	}
+}
