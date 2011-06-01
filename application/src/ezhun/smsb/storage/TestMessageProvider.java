f6610b7b9c11b1e24730be478fa0c98cb116c5f8
Max Korenkov <mkorenkov@gmail.com>
2011-06-01 16:43:41 +0300

    TestMessageProvider reformat code

--- application/src/ezhun/smsb/storage/TestMessageProvider.java
+++ application/src/ezhun/smsb/storage/TestMessageProvider.java
@@ -2,171 +2,173 @@ package ezhun.smsb.storage;
 
 import ezhun.smsb.SmsPojo;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
 
- class TestMessageProvider implements IMessageProvider{
-    ArrayList<SmsPojo> mList;
-    Hashtable<SmsPojo, SmsAction> mActions;
-    int mUnreadCount = 0;
+public class TestMessageProvider implements IMessageProvider {
+	private ArrayList<SmsPojo> mList;
+	private Hashtable<SmsPojo, SmsAction> mActions;
+	private int mUnreadCount = 0;
 
-    public TestMessageProvider(){
-        mActions = new Hashtable<SmsPojo, SmsAction>();
-        mList = new ArrayList<SmsPojo>();
-        Calendar c = Calendar.getInstance();
+	public TestMessageProvider() {
+		mActions = new Hashtable<SmsPojo, SmsAction>();
+		mList = new ArrayList<SmsPojo>();
+		Calendar c = Calendar.getInstance();
 
-        SmsPojo sms = new SmsPojo();
-        sms.setMessage("Hey you there! How you doin'?");
-        sms.setSender("+380971122333");
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		SmsPojo sms = new SmsPojo();
+		sms.setMessage("Hey you there! How you doin'?");
+		sms.setSender("+380971122333");
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("Nova aktsia vid magazinu Target! Kupuy 2 kartopli ta otrymay 1 v podarunok!");
-        sms.setSender("TARGET");
-        c.add(Calendar.SECOND, -30);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("Nova aktsia vid magazinu Target! Kupuy 2 kartopli ta otrymay 1 v podarunok!");
+		sms.setSender("TARGET");
+		c.add(Calendar.SECOND, -30);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("This is my new number. B. Obama.");
-        sms.setSender("+1570333444555");
-        c.add(Calendar.MINUTE, -5);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("This is my new number. B. Obama.");
+		sms.setSender("+1570333444555");
+		c.add(Calendar.MINUTE, -5);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("How about having some beer tonight? Stranger.");
-        sms.setSender("+380509998887");
-        sms.setRead(true);
-        c.add(Calendar.MINUTE, -30);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("How about having some beer tonight? Stranger.");
+		sms.setSender("+380509998887");
+		sms.setRead(true);
+		c.add(Calendar.MINUTE, -30);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("Vash rakhunok na 9.05.2011 stanovyt 27,35 uah.");
-        sms.setSender("KYIVSTAR");
-        c.add(Calendar.MINUTE, -20);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("Vash rakhunok na 9.05.2011 stanovyt 27,35 uah.");
+		sms.setSender("KYIVSTAR");
+		c.add(Calendar.MINUTE, -20);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("Skydky v ALDO. Kupuy 1 ked ta otrymuy dryguy v podarunok!");
-        sms.setSender("ALDO");
-        c.add(Calendar.HOUR, -2);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("Skydky v ALDO. Kupuy 1 ked ta otrymuy dryguy v podarunok!");
+		sms.setSender("ALDO");
+		c.add(Calendar.HOUR, -2);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("Big football tonight v taverne chili!");
-        sms.setSender("+380991001010");
-        sms.setRead(true);
-        c.add(Calendar.HOUR, -30);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("Big football tonight v taverne chili!");
+		sms.setSender("+380991001010");
+		sms.setRead(true);
+		c.add(Calendar.HOUR, -30);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("15:43 ZABLOKOVANO 17,99 UAH, SUPERMARKET PORTAL");
-        sms.setSender("PUMB");
-        c.add(Calendar.HOUR, -400);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("15:43 ZABLOKOVANO 17,99 UAH, SUPERMARKET PORTAL");
+		sms.setSender("PUMB");
+		c.add(Calendar.HOUR, -400);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        sms = new SmsPojo();
-        sms.setMessage("Vash rakhunok na 8.05.2011 stanovyt 24 uah.");
-        sms.setSender("KYIVSTAR");
-        sms.setRead(true);
-        c.add(Calendar.MINUTE, -20);
-        sms.setReceived(c.getTime().getTime());
-        mList.add(sms);
+		sms = new SmsPojo();
+		sms.setMessage("Vash rakhunok na 8.05.2011 stanovyt 24 uah.");
+		sms.setSender("KYIVSTAR");
+		sms.setRead(true);
+		c.add(Calendar.MINUTE, -20);
+		sms.setReceived(c.getTime().getTime());
+		mList.add(sms);
 
-        mUnreadCount = 6;
-    }
+		mUnreadCount = 6;
+	}
 
-    public ArrayList<SmsPojo> getMessages() {
-         return mList;
-    }
+	public ArrayList<SmsPojo> getMessages() {
+		return mList;
+	}
 
-    public Hashtable<SmsPojo, SmsAction> getActionMessages(){
-         return mActions;
-    }
+	public Hashtable<SmsPojo, SmsAction> getActionMessages() {
+		return mActions;
+	}
 
-    public void delete(long id){
-        mActions.clear();
-        SmsPojo sms = get(id);
-        mActions.put(sms, SmsAction.Deleted);
-        mList.remove((int)id);
-    }
+	public void delete(long id) {
+		mActions.clear();
+		SmsPojo sms = get(id);
+		mActions.put(sms, SmsAction.Deleted);
+		mList.remove((int) id);
+	}
 
-    public void delete(long[] ids) {
-        mActions.clear();
-        for(long id = ids.length - 1; id >= 0; id --){
-            SmsPojo sms = get(id);
-            if(!sms.wasRead())
-                mUnreadCount--;
-            mActions.put(sms, SmsAction.Deleted);
-            mList.remove((int)id);
-        }
-    }
+	public void delete(long[] ids) {
+		mActions.clear();
+		for (long id = ids.length - 1; id >= 0; id--) {
+			SmsPojo sms = get(id);
+			if (!sms.wasRead())
+				mUnreadCount--;
+			mActions.put(sms, SmsAction.Deleted);
+			mList.remove((int) id);
+		}
+	}
 
-    public void deleteAll() {
-        for(SmsPojo sms : mList){
-            mActions.put(sms, SmsAction.Deleted);
-        }
-        mList.clear();
-    }
+	public void deleteAll() {
+		for (SmsPojo sms : mList) {
+			mActions.put(sms, SmsAction.Deleted);
+		}
+		mList.clear();
+	}
 
-    public void notSpam(long id){
-        mActions.clear();
-        SmsPojo sms = get(id);
-        mActions.put(sms, SmsAction.MarkedAsNotSpam);
-        mList.remove((int)id);
-    }
+	public void notSpam(long id) {
+		mActions.clear();
+		SmsPojo sms = get(id);
+		mActions.put(sms, SmsAction.MarkedAsNotSpam);
+		mList.remove((int) id);
+	}
 
-    public void notSpam(long[] ids) {
-        mActions.clear();
-        for(long id = ids.length - 1; id >= 0; id --){
-            SmsPojo sms = get(id);
-            if(!sms.wasRead())
-                mUnreadCount--;
-            mActions.put(sms, SmsAction.MarkedAsNotSpam);
-            mList.remove((int)id);
-        }
-    }
+	public void notSpam(long[] ids) {
+		mActions.clear();
+		for (long id = ids.length - 1; id >= 0; id--) {
+			SmsPojo sms = get(id);
+			if (!sms.wasRead())
+				mUnreadCount--;
+			mActions.put(sms, SmsAction.MarkedAsNotSpam);
+			mList.remove((int) id);
+		}
+	}
 
-    public SmsPojo getMessage(long id) {
-        return get(id);
-    }
+	public SmsPojo getMessage(long id) {
+		return get(id);
+	}
 
-    public void read(long id){
-        if (!get(id).wasRead()){
-            get(id).setRead(true);
-            mUnreadCount --;
-        }
-    }
+	public void read(long id) {
+		if (!get(id).wasRead()) {
+			get(id).setRead(true);
+			mUnreadCount--;
+		}
+	}
 
-    public int getUnreadCount(){
-        return mUnreadCount;
-    }
+	public int getUnreadCount() {
+		return mUnreadCount;
+	}
 
-    public void undo(){
-        for(SmsPojo sms : mActions.keySet()){
-            mList.add(0, sms);
-        }
-        mActions.clear();
+	public void undo() {
+		for (SmsPojo sms : mActions.keySet()) {
+			mList.add(0, sms);
+		}
+		mActions.clear();
 
-        mUnreadCount = 0;
-        for(SmsPojo sms : mList){
-            if(!sms.wasRead()){
-                mUnreadCount++;
-            }
-        }
-    }
+		mUnreadCount = 0;
+		for (SmsPojo sms : mList) {
+			if (!sms.wasRead()) {
+				mUnreadCount++;
+			}
+		}
+	}
 
-    public void performActions() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+	public void performActions() {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
 
-    private SmsPojo get(long id){
-        return mList.get((int)id);
-    }
+	private SmsPojo get(long id) {
+		return mList.get((int) id);
+	}
 }
