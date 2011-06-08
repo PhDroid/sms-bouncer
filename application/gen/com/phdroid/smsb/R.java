49f498047aec1a1a83c8539166eb4a114c9a4498
Max Korenkov <mkorenkov@gmail.com>
2011-06-08 12:12:33 +0300

    moved phdroid.test.blackjack package separate

--- application/gen/com/phdroid/smsb/R.java
+++ application/gen/com/phdroid/smsb/R.java
@@ -13,15 +13,13 @@ public final class R {
     public static final class color {
         public static final int button_bg_dark=0x7f040005;
         public static final int button_bg_light=0x7f040004;
-        public static final int green_button_color_shift=0x7f040006;
         public static final int main_list_message_background=0x7f040000;
         public static final int main_list_message_text=0x7f040001;
         public static final int main_list_sender_text=0x7f040002;
-        public static final int message_text=0x7f040008;
-        public static final int received_text=0x7f040009;
-        public static final int red_button_color_shift=0x7f040007;
+        public static final int message_text=0x7f040006;
+        public static final int received_text=0x7f040007;
         public static final int select_many_buttons_background=0x7f040003;
-        public static final int sender_text=0x7f04000a;
+        public static final int sender_text=0x7f040008;
     }
     public static final class drawable {
         public static final int buttons_background=0x7f020000;
